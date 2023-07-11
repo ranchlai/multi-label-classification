beb6c5e93357803dab9bdf9d3222a1707c45f939
ranch <ranchlai@163.com>
2023-07-11 17:15:27 +0800

    add data utils

---
+++ split_data.py
@@ -0,0 +1,32 @@
+
+import random
+import json
+import sys
+import os
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Usage: python split_data.py <input_file>  <output_dir>")
+        sys.exit(1)
+
+    random.seed(42)
+    ifile = sys.argv[1]
+    odir = sys.argv[2]
+    os.makedirs(odir, exist_ok=True)
+    data = json.load(open(ifile))
+    random.shuffle(data)
+    n = len(data)
+    ntrain = int(n * 0.8)
+
+    train = data[:ntrain]
+    val = data[ntrain:]
+
+    with open(os.path.join(odir, "train.json"), "w") as f:
+        json.dump(train, f, indent=2)
+
+    with open(os.path.join(odir, "val.json"), "w") as f:
+        json.dump(val, f, indent=2)
+
+    print(f"train: {len(train)} lines, val: {len(val)} lines")
+    print("done")
---
+++ to_json.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import json
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Usage: python to_json.py <input_file> <output_file>")")
+        sys.exit(1)
+
+
+    ifile=sys.argv[1]
+    ofile=sys.argv[2]
+
+    # read csv file, ignore error lines
+    data=pd.read_csv(ifile, error_bad_lines = False)
+
+    # replace summaries to sentence, terms to label
+    data=data.rename(columns = {"summaries": "sentence", "terms": "label"})
+
+    for i in range(len(data)):
+        label=data["label"][i]
+        "['cs.CV', 'cs.LG']"
+        label=label.replace("\"", '').replace("'", "")
+        label=label.replace("[", "").replace("]", "")
+        label=label.split(",")
+        label=[l.strip() for l in label]
+
+        data["label"][i]=label
+
+    data.to_json(ofile, orient = "records", indent = 2)
+
+    print(f"writed {len(data)} lines to {ofile}")
